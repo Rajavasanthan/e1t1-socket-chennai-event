a7cbffaa3469be089d36e03fad23628c5fd78528
DESKTOP-FOASAOV\muthu <muthukumar@gridsandguides.com>
2018-09-29 15:32:43 +0530

    FB implemented

--- index.js
+++ index.js
@@ -3,22 +3,67 @@ const app = express();
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+const FacebookStatergy = require('passport-facebook').Strategy;
 mongoose.Promise = global.Promise;
 
 mongoose.connect("mongodb://vasanth:vasanth7788@ds119273.mlab.com:19273/e1t1-chennai-socket");
 var { User } = require('./models/user');
+const passport = require('passport');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 var chatSession = session({
-	secret: 'SomeSecretInformation',
-	resave: true,
-	saveUninitialized: true,
-	store: new MongoStore({ mongooseConnection: mongoose.connection })
+    secret: 'SomeSecretInformation',
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
 });
 
 app.use(chatSession);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
+
+
+passport.use(new FacebookStatergy({
+    clientID: process.env.FB_CLIENT_ID || '528230584255234',
+    clientSecret: process.env.FB_SECRET || '527b5d836ce4adc0e453fda88415a939',
+    profileFields: ['email', 'displayName', 'photos'],
+    callbackURL: process.env.FB_CALLBACK || 'http://localhost:3002/auth/facebook/callback',
+    passReqToCallback: true,
+    enableProof: true
+}, (req, token, refreshToken, profile, done) => {
+    User.findOne({ facebook: profile.id }).then((user) => {
+        if (user) {
+            return done(null, user);
+        } else {
+            var newUser = new User({
+                facebook: profile.id,
+                fullName: profile.displayName,
+                email: profile._json.email,
+                userImage: 'https://graph.facebook.com/' + profile.id + '/picture?type=large',
+            });
+            newUser.fbTokens.push({ token: token });
+            newUser.save().then((user) => {
+                done(null, user);
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
+    }).catch((err) => {
+        console.log(err);
+    });
+}));
 
 app.get("/", function (req, res) {
     res.send("Hello World!");
@@ -27,6 +72,15 @@ app.get("/", function (req, res) {
 app.get("/register", function (req, res) {
     res.render("register")
 });
+
+app.get('/auth/facebook', passport.authenticate('facebook', {
+    scope: 'email'
+}));
+
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}));
 
 app.listen(3002, function () {
     console.log("App listening in port 3002")
--- models/user.js
+++ models/user.js
@@ -3,7 +3,22 @@ const mongoose = require('mongoose');
 const UserSchema = mongoose.Schema({
     fullName: {
         type: String
-    }
+    },
+    email: {
+        type: String
+    },
+    status : {
+        type : String
+    },
+    userImage: {
+        type: String,
+        default: 'default.png'
+    },
+    facebook: {
+        type: String,
+        default: ''
+    },
+    fbTokens: Array
 });
 
 var User = mongoose.model("user", UserSchema);
